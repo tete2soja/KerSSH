c9da8b176bdbedc8ec76f308a7af56e8d4d9215f
Nicolas Le Gall <contact@nlegall.fr>
2016-09-22 14:44:25 +0200

    First draft for SSH command from textbox

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management.Automation;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Net;
+using Renci.SshNet;
 
 namespace KerSSH
 {
@@ -20,6 +19,35 @@ namespace KerSSH
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string domain = Domain.GetComputerDomain().Name;
+            string dns = "CICNTP12";
+            PowerShell powerShell = PowerShell.Create();
+            string script = "(Get-DnsServerResourceRecord -ZoneName " + domain + " -RRType A -ComputerName " + dns + " | Where-Object {$_.HostName -like \"disbian*\"}).HostName";
+            powerShell.AddScript(script);
+            var results = powerShell.Invoke();
+
+            foreach (var item in results)
+            {
+                listView1.Items.Add(item.ToString());
+            }
+
+            if (powerShell.Streams.Error.Count > 0)
+            {
+                Console.WriteLine("{0} errors", powerShell.Streams.Error.Count);
+            }
+
+            string server = "";
+            string username = "";
+            string password = "";
+
+            SshClient ssh = new SshClient(server, username, password);
+            ssh.Connect();
+            Console.WriteLine(ssh.RunCommand(textBox1.Text).Result);
+            ssh.Disconnect();
         }
     }
 }
